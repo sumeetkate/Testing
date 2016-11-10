27ece43360a898ba274f6c1a7538a17466b30528
sumeetkate <katesumeet@gmail.com>
2016-11-10 11:03:40 -0500

    adding addtion file

---
+++ HelloWord/src/helloworld/Addtion.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Addtion {
+
+	public static int add(int x, int y) {
+		return x + y;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(add(5, 10));
+	}
+}
